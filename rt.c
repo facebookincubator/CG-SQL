0f98591a406cb4afba25a1dfedf8efd6e8b1f0a4
Rico Mariani <rmariani@fb.com>
2020-08-20 18:07:00 -0700

    Fixing rt.c copyright header

--- rt.c
+++ rt.c
@@ -1,7 +1,9 @@
-// (c) Facebook, Inc. and its affiliates.
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 // These are the various result types we can produce
 // they include useful string fragments for the code generator
