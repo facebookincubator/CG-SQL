db6f23daf18ee80ee8d44d58ba7f6a8f1d81362b
mingodad <mingodad@gmail.com>
2020-10-12 12:18:29 +0200

    Add compiler options to compile with gcc

--- sources/demo/demo.sh
+++ sources/demo/demo.sh
@@ -10,6 +10,6 @@ O="../out"
 CQL="${O}/cql"
 
 ${CQL} --in demo.sql --cg ${O}/demo.h ${O}/demo.c --generate_copy
-cc -g -I.. -I${O} -o ${O}/demo ${O}/demo.c ../cqlrt.c demo_client.c -lsqlite3
+cc -std=c99 -D_Nullable= -D_Nonnull= -g -I.. -I${O} -o ${O}/demo ${O}/demo.c ../cqlrt.c demo_client.c -lsqlite3
 ${O}/demo
 echo "SUCCESS"
