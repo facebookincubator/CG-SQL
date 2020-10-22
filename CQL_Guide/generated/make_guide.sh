d0a0900f8406ed291636c5609456ad04e67483d0
Rico Mariani <rmariani@fb.com>
2020-10-22 13:44:57 -0700

    When making the combined markdown doc for the CQL Guide remove the YAML markings
    Summary:
    When making the combined markdown doc for the CQL Guide remove the YAML markings
    
    This gives us back our nice table of contents and chapter headings in the HTML as well
    and if you the print the HTML you get a better looking PDF.
    
    Note: I'm not landing the updated guide to avoid conflicts as there are other diffs pending.
    
    Reviewed By: RaoulFoaleng
    
    Differential Revision: D24456657
    
    fbshipit-source-id: 2d9d20ce4cfca1520adf4f9e7251253051916293

--- CQL_Guide/generated/make_guide.sh
+++ CQL_Guide/generated/make_guide.sh
@@ -5,10 +5,10 @@
 # LICENSE file in the root directory of this source tree.
 
 rm -f guide.md
-(echo -n "<!--- @" ; echo "generated -->") >guide.md
+(echo -n "<!--- @" ; echo "generated -->") >guide.tmp
 for f in ../ch*.md
 do
-  ( cat "$f"; echo ""; echo "" ) >>guide.md
+  ( cat "$f"; echo ""; echo "" ) >>guide.tmp
 done
 
 for f in ../x*.md
@@ -17,6 +17,11 @@ do
    echo ""; \
    echo ""; \
    echo '<div style="page-break-after: always; visibility: hidden"></div>'; \
-   echo "" ) >>guide.md
+   echo "" ) >>guide.tmp
 done
+
+# remove the docusaurus coding and replace it with simple markdown for the composite guide.
+sed -e '/^id:/d' -e '/^sidebar_label:/d' -e 's/^title: "\(.*\)"/## \1/' -e '/^---$/d' <guide.tmp >guide.md
+rm guide.tmp
+
 pandoc --toc -s -f markdown -t html --metadata title="The CQL Programming Language" guide.md -o guide.html
