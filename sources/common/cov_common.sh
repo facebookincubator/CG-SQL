42b66aedfb2dc070f4a7aaa21f736404829b6d11
Rico Mariani <rmariani@fb.com>
2021-08-30 18:40:15 -0700

    Hoisted common coverage code into cov_common.sh
    Summary:
    This refactor removes duplication between the internal cov.sh script
    and the OSS cov.sh script.
    
    Reviewed By: RaoulFoaleng, winniequinn
    
    Differential Revision: D30657905
    
    fbshipit-source-id: 25aa4c331ff00cf44ab950cd1695c43476064dad

---
+++ sources/common/cov_common.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+OUT_DIR="out"
+
+coverage() {
+  if ! ./test.sh --coverage
+  then
+    echo "you can't run coverage until the tests all pass"
+    return 1
+  fi
+
+  echo generating ${OUT_DIR}/report.html
+  if ! "$@" \
+            --html \
+            --html-details \
+            --object-directory . \
+            -o ${OUT_DIR}/report.html \
+            -e test \
+            -e tester \
+            -e linetester \
+            -e json_test \
+            -e out \
+            -e cql.c \
+            -e cql.y.c \
+            -e cqltest.c \
+            -e result_set_extension.c \
+            -e run_test_client.c \
+            -e query_plan_test.c \
+            -e run_test.c \
+            -e generated_upgrade \
+            -e upgrade
+  then
+    echo "error generating html"
+    return 1
+  fi
+
+  echo generating ${OUT_DIR}/report.txt
+  if ! "$@" \
+              --object-directory . \
+              -o ${OUT_DIR}/report.txt \
+              -e test \
+              -e tester \
+              -e linetester \
+              -e json_test \
+              -e out \
+              -e cql.c \
+              -e cql.y.c \
+              -e cqltest.c \
+              -e result_set_extension.c \
+              -e run_test_client.c \
+              -e query_plan_test.c \
+              -e run_test.c \
+              -e generated_upgrade \
+              -e upgrade
+  then
+    echo "error generating plain text"
+    return 1
+  fi
+
+  return 0
+}
