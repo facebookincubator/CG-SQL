741d59c65a2215fcfbaa427892f7100d74fa6b14
Rico Mariani <ricomariani@outlook.com>
2021-12-28 01:36:39 -0800

    Demo fixes for CQL0435 (#93)
    Summary: Pull Request resolved: https://github.com/facebookincubator/CG-SQL/pull/93
    
    Reviewed By: winniequinn
    
    Differential Revision: D33324771
    
    Pulled By: ricomariani
    
    fbshipit-source-id: aa7b4992a0f64bca8c9f0c4cbca1411e590f8d5f

--- sources/demo/demo.sql
+++ sources/demo/demo.sql
@@ -7,64 +7,71 @@
 
 -- demo stored procs
 
-declare procedure printf no check;
+DECLARE PROC printf NO CHECK;
 
-create procedure make_mixed()
-begin
-  create table mixed(
-    id integer not null,
-    name text,
-    code long int,   -- these are nullable to make the cg harder
-    flag bool,
-    rate real
+CREATE PROC make_mixed ()
+BEGIN
+  CREATE TABLE mixed(
+    id INTEGER NOT NULL,
+    name TEXT,
+    code LONG_INT,
+    flag BOOL,
+    rate REAL
   );
-end;
+END;
 
-create procedure load_mixed()
-begin
-  delete from mixed;
-  insert into mixed values (1, 'a name', 12, 1, 5.0);
-  insert into mixed values (2, 'some name', 14, 3, 7.0);
-  insert into mixed values (3, 'yet another name', 15, 3, 17.4);
-  insert into mixed values (4, 'some name', 19, 4, 9.1);
-  insert into mixed values (5, 'what name', 21, 8, 12.3);
-end;
+CREATE PROC load_mixed ()
+BEGIN
+  DELETE FROM mixed;
+  INSERT INTO mixed VALUES(1, 'a name', 12, 1, 5.0);
+  INSERT INTO mixed VALUES(2, 'some name', 14, 3, 7.0);
+  INSERT INTO mixed VALUES(3, 'yet another name', 15, 3, 17.4);
+  INSERT INTO mixed VALUES(4, 'some name', 19, 4, 9.1);
+  INSERT INTO mixed VALUES(5, 'what name', 21, 8, 12.3);
+END;
 
-create procedure update_mixed(id_ int not null, rate_ real not null)
-begin
-  update mixed set rate = rate_ where id = id_;
-end;
+CREATE PROC update_mixed (id_ INTEGER NOT NULL, rate_ REAL NOT NULL)
+BEGIN
+  UPDATE mixed
+  SET rate = rate_
+    WHERE id = id_;
+END;
 
-@attribute(cql:identity=(id, code))
-@attribute(cql:generate_copy)
-create procedure get_mixed(lim integer not null)
-begin
-  select * from mixed order by id limit lim;
-end;
+@ATTRIBUTE(cql:identity=(id, code))
+@ATTRIBUTE(cql:generate_copy)
+CREATE PROC get_mixed (lim INTEGER NOT NULL)
+BEGIN
+  SELECT *
+    FROM mixed
+  ORDER BY id
+  LIMIT lim;
+END;
 
-create procedure mandelbrot()
-begin
- declare C cursor for
-    WITH RECURSIVE
-      xaxis(x) AS (select -2.0 UNION ALL SELECT x+0.05 FROM xaxis WHERE x<1.2),
-      yaxis(y) AS (select -1.0 UNION ALL SELECT y+0.1 FROM yaxis WHERE y<1.0),
-      m(iter, cx, cy, x, y) AS (
-	SELECT 0 iter, x cx, y cy, 0.0 x, 0.0 y FROM xaxis, yaxis
-	UNION ALL
-	SELECT iter+1 iter, cx, cy, x*x-y*y + cx x, 2.0*x*y + cy y FROM m
-	 WHERE (x*x + y*y) < 4.0 AND iter<28
-      ),
-      m2(iter, cx, cy) AS (
-	SELECT max(iter), cx, cy FROM m GROUP BY cx, cy
-      ),
-      a(t) AS (
-	SELECT group_concat( substr(' .+*#', 1 + min(iter/7, 4), 1), '')
-	FROM m2 GROUP BY cy
-      )
-    SELECT group_concat(rtrim(t),"\n") line FROM a;
+CREATE PROC mandelbrot ()
+BEGIN
+  DECLARE C CURSOR FOR WITH RECURSIVE
+  xaxis (x) AS ( SELECT -2.0 UNION ALL SELECT x + 0.05 FROM xaxis WHERE x < 1.2),
+  yaxis (y) AS ( SELECT -1.0 UNION ALL SELECT y + 0.1 FROM yaxis WHERE y < 1.0),
+  m (iter, cx, cy, x, y) AS (
+    SELECT 0 AS iter, x AS cx, y AS cy, 0.0 AS x, 0.0 AS y
+      FROM xaxis, yaxis
+    UNION ALL
+    SELECT iter + 1 AS iter, cx, cy, x * x - y * y + cx AS x, 2.0 * x * y + cy AS y
+      FROM m
+      WHERE m.x * m.x + m.y * m.y < 4.0 AND m.iter < 28),
+  m2 (iter, cx, cy) AS (
+    SELECT max(iter), cx, cy
+     FROM m
+     GROUP BY cx, cy),
+  a (t) AS (
+    SELECT group_concat(substr(' .+*#', 1 + min(iter / 7, 4), 1), '')
+      FROM m2
+      GROUP BY cy)
+  SELECT group_concat(rtrim(t), "\n") AS line
+    FROM a;
 
-   loop fetch C
-   begin
-     call printf("%s\n", C.line);
-   end;
-end;
+  LOOP FETCH C
+  BEGIN
+    CALL printf("%s\n", C.line);
+  END;
+END;
