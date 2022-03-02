ff707da2ec1e88c867967ea554b1b8fa436a4837
Andres Suarez <asuarez@fb.com>
2022-03-01 20:08:07 -0800

    Fix license headers
    Reviewed By: ricomariani
    
    Differential Revision: D34521943
    
    fbshipit-source-id: 0229eb8f38e9dd8baf57cbe76d3fd01fb3a7c02d

--- sources/tester/regen.sh
+++ sources/tester/regen.sh
@@ -3,8 +3,8 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-#
-# @lint-ignore-every LICENSELINT
+
+# @licenselint-loose-mode
 
 set -euo pipefail
 
