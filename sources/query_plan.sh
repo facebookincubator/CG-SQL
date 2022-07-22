20ffa4f06ceeae6762a4c0cb4e58668553bf9514
Rico Mariani <rmariani@fb.com>
2022-07-21 22:07:12 -0700

    Remove use of hg and buck from query_plan.sh for fully usable by anyone version
    Summary: There were some unneeded FB-isms in the script
    
    Reviewed By: RaoulFoaleng
    
    Differential Revision: D38064386
    
    fbshipit-source-id: b86466b82ecc760c6db05d16d0733807cd1f3a7b

--- sources/query_plan.sh
+++ sources/query_plan.sh
@@ -4,64 +4,75 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-FBSOURCE_DIR=$(realpath "$(hg root)")
-CQL_DIR="${FBSOURCE_DIR}/xplat/msys/tools/cql"
-OUT_DIR="out"
-# We're using CQL binary from BUCK like that users don't have to deal with
-# setup requirements e.g: CQL compile with bison version >= 3.x
-CQL=$(buck build //xplat/msys/tools/cql:cql_impl --show-full-output | sed 's/[^ ]* //')
-cd "${CQL_DIR}" || exit 1
+DIR="$( dirname -- "$0"; )"
+
+cp "$1" "${DIR}/out/qp_in"
+cd "${DIR}" || exit
+
+if ! make > "out/make.out"
+then
+   echo "CQL build failed"
+   cat "out/make.out"
+   failed
+fi
+
+CQL="out/cql"
 
 #echo C preprocessing
-cc -DCQL_TEST -E -x c "$1" >"${OUT_DIR}/cg_test_query_plan2.sql"
+cc -DCQL_TEST -E -x c "out/qp_in" >"out/cg_test_query_plan2.sql"
 
 # echo semantic analysis
-if ! ${CQL} --sem --ast --dev --in "${OUT_DIR}/cg_test_query_plan2.sql" >"${OUT_DIR}/__temp" 2>"${OUT_DIR}/cg_test_query_plan.err"
+if ! ${CQL} --sem --ast --dev --in "out/cg_test_query_plan2.sql" >"out/__temp" 2>"out/cg_test_query_plan.err"
 then
     echo "CQL semantic analysis returned error"
-    cat "${OUT_DIR}/cg_test_query_plan.err"
+    cat "out/cg_test_query_plan.err"
     failed
 fi
 
 # echo codegen query plan
-if ! ${CQL} --test --dev --cg "${OUT_DIR}/cg_test_query_plan.out" --in "${OUT_DIR}/cg_test_query_plan2.sql" --rt query_plan 2>"${OUT_DIR}/cg_test_query_plan.err"
+if ! ${CQL} --test --dev --cg "out/cg_test_query_plan.out" --in "out/cg_test_query_plan2.sql" --rt query_plan 2>"out/cg_test_query_plan.err"
 then
     echo "CQL codegen query plan error"
-    cat "${OUT_DIR}/cg_test_query_plan.err"
+    cat "out/cg_test_query_plan.err"
     failed
 fi
 
-# echo semantic analysis
-if ! ${CQL} --sem --ast --dev --test --in "${OUT_DIR}/cg_test_query_plan.out" >"${OUT_DIR}/__temp" 2>"${OUT_DIR}/cg_test_query_plan.err"
+# echo semantic analysis of generated code (pre check)
+if ! ${CQL} --sem --ast --dev --test --in "out/cg_test_query_plan.out" >"out/__temp" 2>"out/cg_test_query_plan.err"
 then
-    echo "CQL query plan semantic analysis return error"
-    cat "${OUT_DIR}/cg_test_query_plan.err"
+    echo "CQL query plan semantic analysis returned error"
+    cat "out/cg_test_query_plan.err"
     failed
 fi
 
-# echo validate udf test
-if ! ${CQL} --test --dev --cg "${OUT_DIR}/udf.h" "${OUT_DIR}/udf.c" --in "${OUT_DIR}/cg_test_query_plan.out" --rt udf 2>"${OUT_DIR}/udf.err"
+# build any udfs needed
+if ! ${CQL} --test --dev --cg "out/udf.h" "out/udf.c" --in "out/cg_test_query_plan.out" --rt udf 2>"out/udf.err"
 then
     echo "CQL codegen udf return error"
-    cat "${OUT_DIR}/udf.err"
+    cat "out/udf.err"
     failed
 fi
 
-# echo build query plan c code
-if ! ${CQL} --test --dev --cg "${OUT_DIR}/query_plan.h" "${OUT_DIR}/query_plan.c" --in "${OUT_DIR}/cg_test_query_plan.out" 2>"${OUT_DIR}/query_plan_print.err"
+# build query plan c code
+if ! ${CQL} --test --dev --cg "out/query_plan.h" "out/query_plan.c" --in "out/cg_test_query_plan.out" 2>"out/query_plan_print.err"
 then
     echo "CQL codegen query plan return error"
-    cat "${OUT_DIR}/query_plan_print.err"
+    cat "out/query_plan_print.err"
     failed
 fi
 
-# echo compile query plan code
-if ! make query_plan_test >/dev/null 2>"${OUT_DIR}/query_plan_print.err"
+# compile query plan code with Makefile
+if ! make query_plan_test >"out/make.out" 2>"out/make.err"
 then
-    echo "CQL query plan build fialed"
-    cat "${OUT_DIR}/query_plan_print.err"
+    echo "CQL query plan build failed"
+    echo "stdout"
+    cat "out/make.out"
+    echo "stderr"
+    cat "out/make.err"
     failed
 fi
 
 # echo run query plan in c
-"./${OUT_DIR}/query_plan_test"
+"./out/query_plan_test"
+echo
+echo "done"
